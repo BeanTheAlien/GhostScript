973b559e9178a6b75bd65701f4038e5d756cb4f8
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-09-08 08:24:04 -0500

    Various updates

---
+++ main.js
@@ -0,0 +1,10 @@
+var ghostMemory = { "variables": {}, "functions": {}, "methods": {}, "properties": {}, "types": {}, "classes": {} };
+var ghostVariables = {};
+var ghostFunctions = {};
+var ghostMethods = {};
+var ghostProperties = {};
+var ghostTypes = {};
+var ghostClasses = {};
+
+function preprocess(script) {}
+function compile(script) {}
