4f64480bdf168bc878cea20c9846fced89d94f6f
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-10-08 18:27:33 -0500

    Updated run

--- bin/cmds/run.js
+++ bin/cmds/run.js
@@ -1,5 +1,13 @@
+const child_process = require("child_process");
+
 function run(args) {
-    console.log("run");
+    const fileName = args[0];
+    if(!fileName) return console.error("File name required.");
+    child_process.exec(`node ${fileName}`, (err, stdout, stderr) => {
+        if(err) return console.error(`exec error: ${err}`);
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+    });
 }
 
 module.exports = { default: run };
