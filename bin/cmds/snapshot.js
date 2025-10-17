0badbbccc3fec9f79de55a68ed160484ee7d02b4
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-10-17 13:24:02 -0500

    Started implementing snapshot

--- bin/cmds/snapshot.js
+++ bin/cmds/snapshot.js
@@ -1,4 +1,13 @@
 const fs = require("fs");
 const path = require("path");
 
-const dir = path.join(__dirname, "../..");
+function snapshot(args) {
+    const dir = path.join(__dirname, "../..", "snapshots");
+    const file = args[0];
+    const date = new Date();
+    const now = date.now();
+    if(file) {
+        const content = fs.readFile(file);
+        fs.writeFile(path.join(dir, `SNAPSHOT_${file}_${now}`));
+    }
+}
