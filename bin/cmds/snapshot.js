5d4c176770ab6c7495a14502e892a1bdf2f80416
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-10-17 14:49:57 -0500

    Updated snapshot

--- bin/cmds/snapshot.js
+++ bin/cmds/snapshot.js
@@ -4,12 +4,11 @@ const path = require("path");
 function snapshot(args) {
     const dir = path.join(__dirname, "../..", "snapshots");
     const file = args[0];
+    if(!file) return console.error("Cannot snapshot, missing file.")
     const date = new Date();
     const now = date.now();
-    if(file) {
-        const content = fs.readFile(file);
-        fs.writeFile(path.join(dir, `SNAPSHOT-${file}-${now}`), content);
-    } else {
-        const files = fs.readdir(path.join(__dirname, "../.."));
-    }
+    const content = fs.readFile(file);
+    fs.writeFile(path.join(dir, `snapshot-${file}-${now}`), content);
 }
+
+module.exports = { default: snapshot };
