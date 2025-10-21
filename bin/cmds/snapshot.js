af602d0822ccfe9e61d7f1ff11eeaa0f0aac157a
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-10-20 21:16:54 -0500

    Added a missing ;

--- bin/cmds/snapshot.js
+++ bin/cmds/snapshot.js
@@ -5,7 +5,7 @@ function snapshot(args) {
     const dir = path.join(__dirname, "../..", "snapshots");
     if(!fs.existsSync(dir)) fs.mkdirSync(dir);
     const file = args[0];
-    if(!file) return console.error("Cannot snapshot, missing file.")
+    if(!file) return console.error("Cannot snapshot, missing file.");
     const date = new Date();
     const now = date.toISOString().replace(/:/g, "-").replace(/\./, "-").replace("T", "_").replace("Z", "");
     const content = fs.readFileSync(file, "utf8");
