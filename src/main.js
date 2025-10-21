56066515400afdf4cf11d475abe7b1135d641b3d
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-10-20 20:29:21 -0500

    Major upgrade to import logic

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
+var runtime = {};
+
 async function main() {
     const file = "test";
     const json = fs.readFileSync("grammar.json", "utf8");
@@ -28,6 +30,43 @@ async function compile(tokens) {
         const token = tokens[i];
         console.log(token);
     }
+}
+
+async function getModule(name, subname) {
+    const url = `https://beanthealien.github.io/ghost/modules/${name}/${subname}.js`;
+    const mod = require(url);
+    const flat = {};
+    function flatten(arr, k) {
+        if(!Array.isArray(arr)) return;
+        for(const m of arr) {
+            const nk =
+                m.gsVarName ||
+                m.gsFuncName ||
+                m.gsMethodName ||
+                m.gsClassName ||
+                m.gsTypeName ||
+                m.gsPropName ||
+                m.gsModifierName ||
+                m.gsOperatorName ||
+                m.gsErrorName ||
+                m.gsEventName;
+            if(nk) flat[nk] = m;
+        }
+    }
+    flatten(mod.vars, "vars");
+    flatten(mod.funcs, "funcs");
+    flatten(mod.methods, "methods");
+    flatten(mod.classes, "classes");
+    flatten(mod.types, "types");
+    flatten(mod.props, "props");
+    flatten(mod.mods, "mods");
+    flatten(mod.errors, "errors");
+    flatten(mod.events, "events");
+    flatten(mod.operators, "operators");
+    return {
+        meta: mod.ghostmodule,
+        exports: flat
+    };
 }
 
 // var ghostMemory = { "variables": {}, "functions": {}, "methods": {}, "properties": {}, "types": {}, "classes": {} };
