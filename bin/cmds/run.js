9a5cfe0d1e2aaa7373a478d9c73e9b33cf070cde
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-10-23 11:30:10 -0500

    Fixed to use the proper .gst extension

--- bin/cmds/run.js
+++ bin/cmds/run.js
@@ -3,7 +3,7 @@ const child_process = require("child_process");
 function run(args) {
     const fileName = args[0];
     if(!fileName) return console.error("File name required.");
-    child_process.exec(`node ${fileName}`, (err, stdout, stderr) => {
+    child_process.exec(`node ${fileName}.gst`, (err, stdout, stderr) => {
         if(err) return console.error(`exec error: ${err}`);
         if(stderr.length) console.error(stderr);
         else console.log(stdout);
