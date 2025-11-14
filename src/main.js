684a789903ade0931b9affa678725ee1e7742a1b
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-11-14 07:45:27 -0600

    Fixed to push require and exports

--- src/main.js
+++ src/main.js
@@ -536,8 +536,8 @@ async function getModule(name, subname) {
     const url = `https://raw.githubusercontent.com/BeanTheAlien/BeanTheAlien.github.io/main/ghost/modules/${name}/${subname}.js`;
     const js = await fetchRaw(url);
     const module = { exports: {} };
-    const wrapped = new Function(js);
-    await wrapped();
+    const wrapped = new Function("require", "exports", js);
+    await wrapped(require, module.exports);
     // try {
     //     const wrapped = new Function("module", "exports", "require", js);
     //     wrapped(module, module.exports, require);
