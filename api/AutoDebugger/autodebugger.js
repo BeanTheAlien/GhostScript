81abbcd7df14abccf561403c22306e6df470bd1d
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-12-08 10:56:47 -0600

    Added case mismatch detection for autodebugger

--- api/AutoDebugger/autodebugger.js
+++ api/AutoDebugger/autodebugger.js
@@ -7,7 +7,12 @@ class AutoDebuggerAPI {
         this.tokens = tokens;
         this.runtime = runtime;
     }
-    resolveNotFound(input) {}
+    resolveNotFound(input) {
+        // test casing mismatch
+        const keys = Object.keys(this.runtime.scope);
+        const lower = input.toLowerCase();
+        if(keys.map(k => k.toLowerCase()).includes(lower)) return keys.find(k => k.toLowerCase() == lower);
+    }
 }
 
 module.exports = { AutoDebuggerAPI };
