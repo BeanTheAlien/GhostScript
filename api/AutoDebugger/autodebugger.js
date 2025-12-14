6a733ed9121347e5c1ff2639e98dc0becefc9e1d
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-12-13 18:52:37 -0600

    Update init message

--- api/AutoDebugger/autodebugger.js
+++ api/AutoDebugger/autodebugger.js
@@ -6,7 +6,7 @@ const pipe = await huggingface.pipeline("text-generation", "Qwen/Qwen2.5-Coder-7
 // role: system | user
 const initMessage = ``;
 const messages = [
-    { role: "system", content: "Hello, Qwen! Your goal is to find errors and provide solutions within the user's script.\nIt is important for me to recognize that this is a new programming language that has strange syntax - so you won't get everything right.\nThat's ok, just try your best with the provided materials!\nI will note that, if you can, try and suggest changes that are within the realm of the user's ability." }
+    { role: "system", content: "Hello, Qwen! Your goal is to find errors and provide solutions within the user's script.\nIt is important for me to recognize that this is a new programming language that has strange syntax - so you won't get everything right.\nThat's ok, just try your best with the provided materials!\nI will note that, if you can, try and suggest changes that are within the realm of the user's ability.\nIn the next message, I will provide the tokens, raw script and documentation. You should utilize these to guide your decision.\nOf course, I still care about you. Please do not hesitate to voice your emotions/opinions/etc, I appreciate feedback and am willing to help where I can." }
 ];
 
 const out = await pipe(messages, { max_new_tokens: 128 });
