5aebb03a6a26b6c437d899236e2f343d81a07b4b
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-03-27 14:35:49 -0500

    add decs

--- src/module_dev.d.ts
+++ src/module_dev.d.ts
@@ -1,6 +1,8 @@
-type Attach = GSType | GSType[];
+type MbArr<T> = T | T[];
+type Attach = MbArr<GSType>;
+type Mods = MbArr<GSMod>;
 interface GSVarData {
-    gsVarMods: GSModifier[];
+    gsVarMods: GSMod[];
     gsVarType: GSType;
     gsVarDesire: boolean;
     gsVarName: string;
@@ -37,19 +39,34 @@ interface GSTypeData {
     gsTypeName: string;
     gsTypeTest: <T>(v: T) => boolean;
 }
-interface GSModifierData {
-    gsModifierAttach: Attach;
-    gsModifierName: string;
-    gsModifierGet: Function;
-    gsModifierSet: Function;
+interface GSModData {
+    gsModAttach: Attach;
+    gsModName: string;
+    gsModGet: Function;
+    gsModSet: Function;
 }
-interface GSOperatorData {
-    gsOperatorName: string;
-    gsOperatorExec: Function;
+interface GSOprData {
+    gsOprName: string;
+    gsOprExec: Function;
+}
+interface GSDirectiveData {
+    gsDirectiveName: string;
+    gsDirectiveExec: Function;
+}
+interface GSPropData {
+    gsPropName: string;
+    gsPropGet: Function;
+    gsPropSet: Function;
+}
+interface GSArgData {
+    gsArgName: string;
+    gsArgVal: any;
+    gsArgDesire: boolean;
+    gsArgType: GSType;
 }
 
 declare class GSVar implements GSVarData {
-    gsVarMods: GSModifier[];
+    gsVarMods: GSMod[];
     gsVarType: GSType;
     gsVarDesire: boolean;
     gsVarName: string;
@@ -79,101 +96,36 @@ declare abstract class GSType implements GSTypeData {
     gsTypeName: string;
     abstract gsTypeTest: <T>(v: T) => boolean;
 }
-declare class GSModifier implements GSModifierData {
-    gsModifierAttach: Attach;
-    gsModifierName: string;
-    gsModifierGet: Function;
-    gsModifierSet: Function;
+declare class GSMod implements GSModData {
+    gsModAttach: Attach;
+    gsModName: string;
+    gsModGet: Function;
+    gsModSet: Function;
 }
-declare class GSOperator implements GSOperatorData {}
-/**
- * Used for creating operators.
- * @class
- */
-class GSOperator {
-    /**
-     * The constructor for GhostScript operators.
-     * @param {{ gsOperatorName: string, gsOperatorExpression: string[], gsOperatorExec: function, gsOperatorType: GSType, gsOperatorDesire: boolean }} s - The operator settings.
-     */
-    constructor(s) {
-        this.gsOperatorName = s.gsOperatorName;
-        this.gsOperatorExpression = s.gsOperatorExpression;
-        this.gsOperatorExec = s.gsOperatorExec;
-        this.gsOperatorType = s.gsOperatorType;
-        this.gsOperatorDesire = s.gsOperatorDesire;
-    }
+declare class GSOpr implements GSOprData {
+    gsOprName: string;
+    gsOprExec: Function;
 }
-/**
- * Used for creating directives.
- * @class
- */
-class GSDirective {
-    /**
-     * The constructor for GhostScript directives.
-     * @param {{ gsDirectiveName: string, gsDirectiveExec: function }} s - The directive settings.
-     */
-    constructor(s) {
-        this.gsDirectiveName = s.gsDirectiveName;
-        this.gsDirectiveExec = s.gsDirectiveExec;
-    }
+declare class GSDirective implements GSDirectiveData {
+    gsDirectiveName: string;
+    gsDirectiveExec: Function;
 }
-/**
- * Used for creating arguments.
- * @class
- */
-class GSArg {
-    /**
-     * The constructor for GhostScript arguments.
-     * @param {{ gsArgName: string, gsArgVal: Object, gsArgDesire: boolean, gsArgType: GSType }} s - The argument settings.
-     */
-    constructor(s) {
-        this.gsArgName = s.gsArgName;
-        this.gsArgVal = s.gsArgVal;
-        this.gsArgDesire  = s.gsArgDesire;
-        this.gsArgType = s.gsArgType;
-    }
+declare class GSArg implements GSArgData {
+    gsArgName: string;
+    gsArgVal: any;
+    gsArgDesire: boolean;
+    gsArgType: GSType;
 }
-/**
- * Used for creating managers.
- * @class
- */
-class GSManager {
-    /**
-     * The constructor for GhostScript managers.
-     * @param {{ gsManagerName: string, gsManagerVals: Object }} s - The manager settings.
-     */
-    constructor(s) {
-        this.gsManagerName = s.gsManagerName;
-        this.gsManagerVals = s.gsManagerVals;
-    }
-    /**
-     * Gets an entry.
-     * @param {string} name - The entry name.
-     * @returns {Object|undefined} The entry value.
-     */
-    get(name) {
-        return this.gsManagerVals[name];
-    }
-    /**
-     * Sets an entry.
-     * @param {string} name - The entry name.
-     * @param {Object} val - The entry value.
-     */
-    set(name, val) {
-        this.gsManagerVals[name] = val;
-    }
-    /**
-     * Deletes an entry.
-     * @param {string} name - The entry name.
-     */
-    del(name) {
-        delete this.gsManagerVals[name];
-    }
+declare class GSProp implements GSPropData {
+    gsPropDesire: boolean;
+    gsPropAttach: Attach;
+    gsPropName: string;
+    gsPropGet: Function;
+    gsPropSet: Function;
 }
 
-module.exports = {
+export {
     GSVar, GSFunc, GSMethod, GSClass,
-    GSType, GSProp, GSModifier, GSErr,
-    GSEvent, GSGroup, GSOperator,
-    GSDirective, GSArg, GSManager
+    GSType, GSProp, GSMod, GSOpr,
+    GSDirective, GSArg
 };
